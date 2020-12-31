f25dddc393aa9ff0fc99aeb5058aa813e5942ea9
b.gaj <b.gaj@avsystem.com>
2020-12-31 12:51:54 +0100

    Added charts

--- app/src/main/java/pl/edu/agh/mobilesystems/falldetection/MainActivity.kt
+++ app/src/main/java/pl/edu/agh/mobilesystems/falldetection/MainActivity.kt
@@ -4,18 +4,25 @@ import android.content.Context
 import android.graphics.Color
 import android.hardware.SensorManager
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.github.mikephil.charting.charts.LineChart
 import kotlinx.coroutines.*
 import pl.edu.agh.mobilesystems.falldetection.accelerometer.AccelerometerService
 import pl.edu.agh.mobilesystems.falldetection.accelerometer.impl.AccelerometerServiceImpl
+import pl.edu.agh.mobilesystems.falldetection.charts.ChartUtils
 import pl.edu.agh.mobilesystems.falldetection.detection.KNNDetector
 import kotlin.coroutines.CoroutineContext
+
 
 class MainActivity : AppCompatActivity(), CoroutineScope {
     private lateinit var accelerometerService: AccelerometerService
 
     private lateinit var mJob: Job
+    private var isChartActive: Boolean = false;
     override val coroutineContext: CoroutineContext
         get() = mJob + Dispatchers.Main
 
@@ -26,21 +33,44 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
     private lateinit var yCoordinateField: EditText
     private lateinit var zCoordinateField: EditText
     private lateinit var distanceField: EditText
+    private lateinit var chartButton: Button
+    private lateinit var inputsLayout: ConstraintLayout
+    private lateinit var chartsLayout: ConstraintLayout
+    private lateinit var lineChart: LineChart
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mJob = Job()
         accelerometerService =
-                AccelerometerServiceImpl(getSystemService(Context.SENSOR_SERVICE) as SensorManager).also {
-                    it.start()
-                }
+            AccelerometerServiceImpl(getSystemService(Context.SENSOR_SERVICE) as SensorManager).also {
+                it.start()
+            }
         kNNDetector = KNNDetector()
 
         xCoordinateField = findViewById(R.id.xCoord)
         yCoordinateField = findViewById(R.id.yCoord)
         zCoordinateField = findViewById(R.id.zCoord)
         distanceField = findViewById(R.id.distance)
+        lineChart = findViewById(R.id.chart)
+
+        inputsLayout = findViewById(R.id.inputsLayout)
+        chartsLayout = findViewById(R.id.chartsLayout)
+        chartButton = findViewById(R.id.chartButton)
+        chartButton.setOnClickListener {
+            isChartActive = !isChartActive
+            if (inputsLayout.visibility == View.VISIBLE) {
+                inputsLayout.visibility = View.GONE
+                chartsLayout.visibility = View.VISIBLE
+            } else {
+                inputsLayout.visibility = View.VISIBLE
+                chartsLayout.visibility = View.GONE
+                lineChart.data.clearValues();
+                lineChart.clear()
+            }
+        }
+
+
 
         updateCoordJob = launch {
             while (true) {
@@ -56,6 +86,15 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
                 yCoordinateField.setText(String.format("%.2f", coordinates.yCoordinate))
                 zCoordinateField.setText(String.format("%.2f", coordinates.zCoordinate))
                 delay(100)
+                if (isChartActive) {
+                    ChartUtils.addEntry(
+                        lineChart,
+                        coordinates.xCoordinate,
+                        coordinates.yCoordinate,
+                        coordinates.zCoordinate,
+                        distance
+                    )
+                }
             }
         }
     }
